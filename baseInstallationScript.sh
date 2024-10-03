be9e6bc68e775d32ea8faaaf16cfddec1f6518cb
Dekta92 <delt4029@gmail.com>
2024-10-03 21:17:46 +0500

    Improved baseInstallationScript.sh

--- baseInstallationScript.sh
+++ baseInstallationScript.sh
@@ -1,6 +1,6 @@
 sudo timedatectl set-timezone Etc/GMT-5
 
-#
+
 read -p "Do you want to set up any partitions first? (y/N): " response
 
 response=${response,,}
@@ -17,14 +17,18 @@ else
     exit 1
 fi
 
+clear
 
+echo -e "\033[1;37mWhat will be your main drive for Arch? (e.g. /dev/sda, /dev/sdb1)\033[0m"
 fdisk -l | grep /dev/sd | awk '{print "\033[1;37m" $0 "\033[0m"}'
-read -p "Please enter the main installation drive (e.g., /dev/sda): " main_drive
+read -p "Enter your choice: " main_drive
 echo "You have selected: $main_drive"
 
+echo "Now formatting $main_drive in 5 seconds (Press Ctrl + C to abort script)"
+sleep 5
 mkfs.ext4 "${main_drive}1"
 
+mount $main_drive /mnt
 genfstab /mnt > /mnt/etc/fstab
 arch-chroot /mnt bash -c 'pacman -Sy grub; grub-install $main_drive; grub-mkconfig -o /boot/grub/grub.cfg'
 reboot
-
