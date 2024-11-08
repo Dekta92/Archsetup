28952ea93df9d966f3522c0d56587282bffced61
Dekta92 <delt4029@gmail.com>
2024-11-08 20:49:45 +0500

    Commented out some unnecessary lines

--- startupscript.sh
+++ startupscript.sh
@@ -1,9 +1,9 @@
 alias neofetch='fastfetch'
 alias desktop='startxfce4'
 
-# Making sure Wi-Fi adapter is on
-rfkill unblock wlan0
-iwctl device wlan0 set-property Powered on
+## Making sure Wi-Fi adapter is on
+#rfkill unblock wlan0
+#iwctl device wlan0 set-property Powered on
 
 clear
 fastfetch
